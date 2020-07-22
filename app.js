6321c206915613c2163aef78cb731e89bca5a07b
calinovidiu96 <calinovidiu96@gmail.com>
2020-07-22 17:54:05 +0300

    e

--- app.js
+++ app.js
@@ -141,32 +141,20 @@ function callSendAPI(sender_psid, response) {
   }
 
   //Send the HTTP request to the Messenger Platform
-  // request({
-  //   uri: "https://graph.facebook.com/v2.6/me/messages",
-  //   qs: { "access_token": PAGE_ACCESS_TOKEN },
-  //   method: 'POST',
-  //   json: request_body
-  // }, (err, res, body) => {
-  //   if (!err) {
-  //     console.log('message sent!')
-  //   } else {
-  //     console.error("Unable to send message:" + err);
-  //   }
-  // }); 
+  request({
+    uri: "https://graph.facebook.com/v2.6/me/messages",
+    qs: { "access_token": PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 
 
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: AGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: request_body
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
 
    
 }
