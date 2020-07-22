3be2ac5a78fb83d0ea554728541d32d529c254c1
calinovidiu96 <calinovidiu96@gmail.com>
2020-07-22 17:52:24 +0300

    e

--- app.js
+++ app.js
@@ -24,7 +24,7 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
 const 
-  axios = require('request-promise'),
+  axios = require('request'),
   express = require('express'),
   body_parser = require('body-parser'),
   app = express().use(body_parser.json()); // creates express http server
@@ -141,21 +141,32 @@ function callSendAPI(sender_psid, response) {
   }
 
   //Send the HTTP request to the Messenger Platform
-  request({
-    uri: "https://graph.facebook.com/v2.6/me/messages",
-    qs: { "access_token": PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  }); 
+  // request({
+  //   uri: "https://graph.facebook.com/v2.6/me/messages",
+  //   qs: { "access_token": PAGE_ACCESS_TOKEN },
+  //   method: 'POST',
+  //   json: request_body
+  // }, (err, res, body) => {
+  //   if (!err) {
+  //     console.log('message sent!')
+  //   } else {
+  //     console.error("Unable to send message:" + err);
+  //   }
+  // }); 
 
 
-
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: AGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: request_body
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
 
    
 }
