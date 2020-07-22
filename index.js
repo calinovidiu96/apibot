878bd30311248836a8aedad8329f825c108b4486
calinovidiu96 <calinovidiu96@gmail.com>
2020-07-22 12:46:51 +0300

    a

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 
 //Creates the endpoint for our webhook
-app.post('/webhook', (req, res) => {
+app.post('', (req, res) => {
     
     let body = req.body;
 
