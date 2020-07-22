20f667eb45ccd9736fff48abceb04ece925233f7
calinovidiu96 <calinovidiu96@gmail.com>
2020-07-22 16:07:51 +0300

    e

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post('', (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.messages) {
-        handleMessage(sender_psid, webhook_event.messages);        
+        handleMessage(sender_psid, webhook_event.message);        
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);
       }
