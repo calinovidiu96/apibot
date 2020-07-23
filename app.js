f058dbd8c5ca8cf3a2d2b01410936668cf7c5c75
calinovidiu96 <calinovidiu96@gmail.com>
2020-07-23 09:57:24 +0300

    e

--- app.js
+++ app.js
@@ -136,28 +136,28 @@ function callSendAPI(sender_psid, response) {
   }
 
   // Send the HTTP request to the Messenger Platform
-  // request({
-  //   "uri": "https://graph.facebook.com/v2.6/me/messages",
-  //   "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-  //   "method": "POST",
-  //   "json": request_body
-  // }, (err, res, body) => {
-  //   if (!err) {
-  //     console.log('message sent!')
-  //   } else {
-  //     console.error("Unable to send message:" + err);
-  //   }
-  // }); 
-
-  const postToFacebook = {  
-    method: 'POST',
-    uri: `https://graph.facebook.com/v2.6/me/messages?hub.verify_token=EAAMi8ezOnHEBAFMO2leqrLtcwN2I228g3KHAZB5h2a1IMZCbVSoeg3wUfaILXvTcmJEYfa8uBgb2ni2dWtiDke7qyXMtJfsdWYiheu5FXo2PDB6tFUo9xJoeanVM1Fhc3zHsslsb95c4llCSQYMlLZBa8rN6HjgZATmyBrfmtwZDZD`,
-    qs: {
-     
-      json: request_body
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "bearer": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
     }
-  };
-  request(postToFacebook)
+  }); 
+
+//   const postToFacebook = {  
+//     method: 'POST',
+//     uri: `https://graph.facebook.com/v2.6/me/messages?hub.verify_token=EAAMi8ezOnHEBAFMO2leqrLtcwN2I228g3KHAZB5h2a1IMZCbVSoeg3wUfaILXvTcmJEYfa8uBgb2ni2dWtiDke7qyXMtJfsdWYiheu5FXo2PDB6tFUo9xJoeanVM1Fhc3zHsslsb95c4llCSQYMlLZBa8rN6HjgZATmyBrfmtwZDZD`,
+//     qs: {
+     
+//       json: request_body
+//     }
+//   };
+//   request(postToFacebook)
     
 }
 
